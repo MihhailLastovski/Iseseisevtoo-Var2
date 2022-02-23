4178afa915c8deccd82bbb776943e5a2fbac14cf
opilane <opilane@HP-CZC6297BB4>
2022-02-23 10:05:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iseseisevtoo_Var2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //1
+            //TasksVar2.task1();
+            //2
+            //TasksVar2.task2();
+            //3
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TasksVar2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iseseisevtoo_Var2
+{
+    class TasksVar2
+    {
+        public static void task1() 
+        {
+            string slov = Console.ReadLine();
+            Console.Write("Количество слов => ");
+            Console.WriteLine(slov.Split(' ').Length);
+            Console.Write("Количество пробелов => ");
+            Console.Write(slov.Split(' ').Length - 1);
+            Console.ReadKey();
+            Console.ReadKey(true);
+        }
+        public static void vivodmassiva(Array n) 
+        {
+            Array.Sort(n);
+            foreach (int item in n)
+            {
+                if (item == 0) //if сделан для того, чтобы не выписывались 0 из массива, которые там изначально есть из-за указания его размера
+                {
+
+                }
+                else
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+        public static void task2()
+        {
+            Random rnd = new Random();
+            int[] number = new int[20];
+            int[] number_chet = new int[20];
+            int[] number_nechet = new int[20];
+            int d = 0;
+            int c = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                number[i] = rnd.Next(1, 20);
+            }
+            foreach (int item in number)
+            {
+                if (item % 2 == 0)
+                {
+                    number_chet[d] = item;
+                    d++;
+                }
+
+                else
+                {
+                    number_nechet[c] = item;
+                    c++;
+                }
+            }
+            TasksVar2.vivodmassiva(number_chet);
+            TasksVar2.vivodmassiva(number_nechet);
+        }
+    }
+}
